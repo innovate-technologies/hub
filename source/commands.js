3335df2ae19ac04b099d49bf4af9d2ff477c9326
Léo Lam <leo@innovatetechnologi.es>
2016-08-22 15:54:13 +0200

    Commands: Add an update command

--- source/commands.js
+++ source/commands.js
@@ -7,6 +7,7 @@ import config from "config";
 import * as events from "app/events.js";
 import log from "app/logs.js";
 import { SlackMessageEvent, sendMessage } from "app/slack.js";
+import { exec } from "app/utils.js";
 
 const TRUSTED_PEOPLE: Array<string> = config.get("slack.trustedPeople");
 
@@ -16,6 +17,18 @@ events.listen(SlackMessageEvent.name, async (event: SlackMessageEvent) => {
   }
 
   const trusted: bool = TRUSTED_PEOPLE.includes(event.from);
+
+  if (trusted && event.message.endsWith("update")) {
+    log.info("Running git pull --rebase");
+    try {
+      const output = await exec("git", ["pull", "--rebase"]);
+      await sendMessage(event.channel, "Done, here is the output of git pull:\n" + output);
+    } catch (error) {
+      log.error(error);
+      await sendMessage(event.channel, "I couldn't do that.");
+    }
+    return;
+  }
 
   if (trusted && event.message.endsWith("restart")) {
     await sendMessage(event.channel, "See ya all later byeeeee");
--- source/utils.js
+++ source/utils.js
@@ -2,7 +2,18 @@
 // This file is part of hub. Refer to license.txt for more information.
 // @flow
 
+import { execFile } from "child_process";
 import util from "util";
+
+export const exec = (binary: string, args: Array<string>, options: Object = {}) =>
+  new Promise((resolve: Function, reject: Function) => {
+    execFile(binary, args, options, (err: ?Error, stdout: Buffer) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(stdout);
+    });
+  });
 
 export const objectToString = (...args: any): string => {
   return util.inspect(...args, { depth: null, maxArrayLength: null, breakLength: Infinity });
